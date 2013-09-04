f63d182b9647e5fb4c8687bcfd66079b27aad31d
Lee <okeefe.lee@gmail.com>
2013-09-04 16:02:21 -0700

    change the way a tag name is found

--- indent.js
+++ indent.js
@@ -1,12 +1,14 @@
 // takes a string of html code and tries to tab it properly
 // very basic and will break with bad html
-var indent = function(str){
+// TODO: ignore javascript and css (will need to change the replace at the begining)
+//   maybe remove line breaks as we step over the html instead of in the begining?
+// TODO: handle single line empty divs as inline - EX: <div class="something"></div>
+var indentHtml = function(str){
   var tabSize = 2;
   var loc = 0;
   var depth = 0;
   var carCount = 0;
   var out = "";
-  var maxTagLength = 8;
   var afterClosed = true;
   var lastWasInline = false;
   var inlineElements = "b big i small tt abbr acronym cite code dfn em kbd strong samp var a bdo br img map object q script span sub sup button input label textarea".split(' '); // removed from list: select
@@ -20,7 +22,13 @@ var indent = function(str){
     return false;
   };
   var getElementTag = function(loc, str){
-    return str.slice((loc+tagStart),(loc+maxTagLength+tagStart)).split(/\s|\n|\>/).shift();
+    var tagLoc = loc;
+    var tag = '';
+    while(!(/\s|\r|\n|\>|\//.test(str[tagLoc]))){
+      tag += str[tagLoc];
+      tagLoc++;
+    }
+    return tag;
   }
   var getTab = function(){
     var tab = '';
@@ -28,14 +36,14 @@ var indent = function(str){
     return tab;
   }
 
-  str = str.replace(/(\n|\r)\s*\</g,"<").replace(/(\n|\r)/g," ").replace(/\s+\</g,"<").replace(/\s+/g," ");
+  str = str.replace(/(\n|\r)\s*\</g,"<").replace(/(\n|\r)/g," ").replace(/\s+\</g,"<");//.replace(/\s+/g," ");
   while(loc < str.length){
     if(str[loc] == '<'){
       carCount++;
       if(carCount < 2){
         var closingTag = (str[loc+1] == '/');
         var tagStart = closingTag ? 2 : 1;
-        var tagName = getElementTag(loc, str);
+        var tagName = getElementTag(loc+tagStart, str);
         var inlineElement = checkTags(tagName, inlineElements);
         var voidElement = checkTags(tagName, voidElements);
 
@@ -59,11 +67,15 @@ var indent = function(str){
 
       if(carCount < 1){
         var selfClosing = (str[loc-1] == '/');
-        var newLoc = loc;
-        while(!/\</.test(str[newLoc])){
-          newLoc--;
-        }
-        var tagName = getElementTag(newLoc, str);
+        var tagName = getElementTag(function(newLoc,str){
+          while(!/\</.test(str[newLoc-1])){
+            newLoc--;
+          }
+          if(str[newLoc]=='/'){
+            newLoc++;
+          }
+          return newLoc;
+        }(loc,str), str);
         var inlineElement = checkTags(tagName, inlineElements);
         var voidElement = checkTags(tagName, voidElements);
 
